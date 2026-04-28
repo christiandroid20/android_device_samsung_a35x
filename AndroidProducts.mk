61a3c5e6b21677e7b51c147e74fae9450ef23017
christiandroid20 <75439616+christiandroid20@users.noreply.github.com>
2026-04-28 14:38:35 -0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,11 @@
 #
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
+# AndroidProducts.mk - TWRP + SHRP (dual support)
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_a35x.mk
+    $(LOCAL_DIR)/twrp_a35x.mk \
+    $(LOCAL_DIR)/omni_a35x.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_a35x-eng 
+    twrp_a35x-eng \
+    omni_a35x-eng
