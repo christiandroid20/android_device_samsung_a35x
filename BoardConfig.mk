3be5633e15428880db15a332f02275314e0a7a2e
christiandroid20 <75439616+christiandroid20@users.noreply.github.com>
2026-04-28 20:18:42 -0600

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -162,6 +162,6 @@ PB_TORCH_PATH := "/sys/devices/virtual/camera/flash/rear_flash"
 PB_TORCH_MAX_BRIGHTNESS := 1
 
 # Version/Maintainer
-TW_DEVICE_VERSION := SavedByLight Stable
-SHRP_MAINTAINER := SavedByLight Pre-Release
-MAINTAINER := SavedByLight
+TW_DEVICE_VERSION := Christygaming18
+SHRP_MAINTAINER := Christygaming18
+MAINTAINER := Christygaming18
