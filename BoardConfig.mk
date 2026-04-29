d4352b8905160ed965d770cf0cea97520d58dbf7
christiandroid20 <75439616+christiandroid20@users.noreply.github.com>
2026-04-28 22:30:05 -0600

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -96,7 +96,7 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 TW_INCLUDE_NTFS_3G := true
 TARGET_USES_MKE2FS := true
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
+TARGET_RECOVERY_FSTAB := device/samsung/a35x/recovery.fstab
 
 # Verified Boot
 BOARD_AVB_ENABLE := false
