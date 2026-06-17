4810d50a620d66d1895fa9df560bc60a74f0dbf5
Christy Vela <75439616+christiandroid20@users.noreply.github.com>
2026-06-17 14:55:25 -0600

    Update twrp_a35x.mk

--- twrp_a35x.mk
+++ twrp_a35x.mk
@@ -4,31 +4,22 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # Inherit some common twrp stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
-
+$(call inherit-product-if-exists, vendor/twrp/config/common.mk)
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-
 # Inherit from a35x device
 $(call inherit-product, device/samsung/a35x/device.mk)
-
 PRODUCT_DEVICE := a35x
 PRODUCT_NAME := twrp_a35x
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-A356B
+PRODUCT_MODEL := SM-A356E
 PRODUCT_MANUFACTURER := samsung
-
-
 #PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/a35x/recovery/root,recovery/root)
-
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images
-
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
